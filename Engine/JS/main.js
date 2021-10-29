a8305f0dfb579a21748c3f834039022f58a4f941
NeMedved <92400587+NeMedved@users.noreply.github.com>
2021-10-29 17:38:24 +0300

    1
    1

--- Engine/JS/main.js
+++ Engine/JS/main.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
         document.getElementById('Ru').addEventListener('click', () => {
             document.getElementById('title').innerHTML = 'Полезные ссылки для связи со мной';
-            document.getElementById('menu').innerHTML = '<a href="https://www.instagram.com/nitorior/?hl=ru" target="_blank">Открыть INST</a><a href="https://www.fiverr.com/nitorior?up_rollout=true" target="_blank">Открыть FIVERR</a><a href="https://vk.com/nikitamiheevv" target="_blank">Открыть VK</a>    <a href="https://portfolio.commishes.com/user/NeMedved/" target="_blank">Открыть YUCH</a>';
+            document.getElementById('menu').innerHTML = '<a href="https://www.instagram.com/nitorior/?hl=ru" target="_blank">Открыть INST</a><a href="https://www.fiverr.com/nitorior?up_rollout=true" target="_blank">Открыть FIVERR</a><a href="https://vk.com/nikitamiheevv" target="_blank">Открыть VK</a> <a href="https://ping.commishes.com/@NeMedved/" target="_blank">Открыть YCH</a>';
         })
 
         document.getElementById('En').addEventListener('click', () => {
             document.getElementById('title').innerHTML = 'Useful links for contacting me';
-            document.getElementById('menu').innerHTML = '<a href="https://www.instagram.com/nitorior/?hl=ru" target="_blank">Open INST</a><a href="https://www.fiverr.com/nitorior?up_rollout=true" target="_blank">Open FIVERR</a><a href="https://vk.com/nikitamiheevv" target="_blank">Open VK</a>    <a href="https://portfolio.commishes.com/user/NeMedved/" target="_blank">Open YUCH</a>';
+            document.getElementById('menu').innerHTML = '<a href="https://www.instagram.com/nitorior/?hl=ru" target="_blank">Open INST</a><a href="https://www.fiverr.com/nitorior?up_rollout=true" target="_blank">Open FIVERR</a><a href="https://vk.com/nikitamiheevv" target="_blank">Open VK</a> <a href="https://ping.commishes.com/@NeMedved/" target="_blank">Open YCH</a>';
         })
 
 })
